beb2c28a77c2cbc1c10b9b599dda6a02710b8d78
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2023-03-27 10:16:59 -0300

    mudei de pasta

--- etec/Agenda 7/Exercício apostila/cadastroAction.php
+++ etec/Agenda 7/Exercício apostila/cadastroAction.php
@@ -20,7 +20,7 @@
         try {
             $conecta = new PDO("mysql:dbname=$bd; host=$host; port=3306; charset=utf8", $usuario, $senha);
             $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conectado";
+            // echo "Conectado";
         } catch (PDOException $e) {
             echo "falha ao conectar: " . $e->getMessage();
         }
