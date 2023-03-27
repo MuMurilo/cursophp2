07c46a1cd4dbac940bf38179ded847fb4a7504cd
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2023-03-27 12:49:01 -0300

    Create lista.php

---
+++ etec/Agenda 7/Você no comando/lista.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome/4.7.0/css/font-awesome.min.css">
+    <title>Lista de Produtos - PDO</title>
+</head>
+
+<body class="w3-black">
+
+    <?php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "060291";
+    $bd = "pwii";
+    try {
+        $conecta = new PDO("mysql:dbname=$bd; host=$host; port=3306; charset=utf8", $usuario, $senha);
+        $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo '<div class="w3-paddingw3-content w3-half w3-displaytopmiddle w3-margin">
+<h1 class="w3-center w3-indigo w3-round-large w3-margin">Lista de Estados</h1>
+<table class="w3-table-all w3-centered w3-text-black">
+<thead>
+<tr class="w3-center w3-indigo">
+<th>Código</th>
+<th>Nome</th>
+<th>Sigla</th>
+</tr>
+<thead>
+';
+        $sql = "SELECT * FROM estado";
+        $resultado = $conecta->query($sql);
+        if ($resultado != null)
+            foreach ($resultado as $linha) {
+                echo '<tr>';
+                echo '<td>' . $linha['idestado'] . '</td>';
+                echo '<td>' . $linha['nome'] . '</td>';
+                echo '<td>' . $linha['sigla'] . '</td>';
+                // echo '<td></td>';
+                echo '</tr>';
+            }
+        echo '
+</table>
+</div>';
+    } catch (PDOException $e) {
+        echo "falha ao conectar: " . $e->getMessage();
+    }
+    ?>
+    </div>
+</body>
+<script src="https://kit.fontawesome.com/ef4189ae1e.js" crossorigin="anonymous"></script>
+</html>
