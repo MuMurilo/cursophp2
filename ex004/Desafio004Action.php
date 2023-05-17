bb70ba14fee7ef96ca89c2fa941a6c1f1f8b5c5c
Murilo Gomes Santos <murilo.gomes.santos@gmail.com>
2023-05-17 05:53:28 -0300

    text

--- ex004/Desafio004Action.php
+++ ex004/Desafio004Action.php
@@ -10,31 +10,32 @@
 </head>
 
 <body>
-<h1>O Seu dinheiro em Dolares é: <i class="fa fa-down-long "></i> </h1>
-<h2>
-<?php 
+    <h1>O Seu dinheiro em Dolares é: <i class="fa fa-down-long "></i> </h1>
+    <h2>
+        <?php
 
 
-$inicio = date ("m-d-Y", strtotime("-7 days"));
-$fim = date("m-d-Y");
-$url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-$dados = json_decode(file_get_contents($url),true);
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+        $dados = json_decode(file_get_contents($url), true);
 
-// var_dump($dados);
+        // var_dump($dados);
 
-$cotacao = $dados["value"][0]["cotacaoCompra"];
+        $cotacao = $dados["value"][0]["cotacaoCompra"];
 
-$num = $_POST['numero'];
-$dol = $num/$cotacao;
-$dol = number_format($dol, 2, ',', ' ');
-echo "O número digitado foi: $num <br>";
-echo 'Seu valor em Reais é: <i class="fa-solid fa-brazilian-real-sign"></i>'. $num;
-echo"<br>";
-echo 'Seu valor em Dolar é: <i class="fa-solid fa-dollar-sign"></i>'. $dol;
-echo"<br>";
-?>
-</h2>
+        $num = $_POST['numero'];
+        $dol = $num / $cotacao;
+        $dol = number_format($dol, 2, ',', ' ');
+        echo "O número digitado foi: $num <br>";
+        echo 'Seu valor em Reais é: <i class="fa-solid fa-brazilian-real-sign"></i>' . $num;
+        echo "<br>";
+        echo 'Seu valor em Dolar é: <i class="fa-solid fa-dollar-sign"></i>' . $dol;
+        echo "<br>";
+        ?>
+    </h2>
 </body>
 <p><a href="javascript:history.go(-1)">Voltar a pagina anterior</a></p>
 <script src="https://kit.fontawesome.com/ef4189ae1e.js" crossorigin="anonymous"></script>
+
 </html>
